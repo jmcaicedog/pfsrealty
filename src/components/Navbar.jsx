28148c07a4a60616f901a408546b0a49f80aeea3
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-03-22 20:00:40 -0500

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FiMenu } from "react-icons/fi";
+
+const Navbar = () => {
+  return;
+  <div>
+    <div className="">
+      <FiMenu />
+    </div>
+  </div>;
+};
+
+export default Navbar;
